9914c5be595584b1b56b6dadfb45764dbc938316
Foorack <max@foorack.com>
2021-09-24 22:58:00 +0200

    ci: add auto-generate

--- generate.sh
+++ generate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+npm install @openapitools/openapi-generator-cli
+
 rm docs -rf
 rm src/main/java/io/github/vrchatapi/*.java
 rm src/main/java/io/github/vrchatapi/api -rf
 rm src/main/java/io/github/vrchatapi/auth -rf
 rm src/main/java/io/github/vrchatapi/model -rf
 
-openapi-generator-cli generate \
+./node_modules/\@openapitools/openapi-generator-cli/main.js generate \
 -g java \
 --additional-properties=groupId=io.github.vrchatapi,apiPackage=io.github.vrchatapi.api,invokerPackage=io.github.vrchatapi,modelPackage=io.github.vrchatapi.model,artifactId=vrchatapi,licenseName=MIT,licenseUrl=https://github.com/vrchatapi/vrchatapi-java/blob/main/LICENSE,developerEmail=me@ruby.js.org,developerName=VRChatAPI,developerOrganization=vrchatapi,developerOrganizationUrl=https://github.com/VRChatAPI,hideGenerationTimestamp=true \
 --git-user-id=vrchatapi \
