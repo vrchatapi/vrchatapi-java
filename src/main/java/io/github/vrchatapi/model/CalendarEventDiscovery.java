28a0ea5fe30bcbd2faf060d0e77ab4ae950c2cc6
ariesclark <ariesclark@users.noreply.github.com>
2026-09-21 01:35:45 +0000

    Upgrade Java SDK to spec 1.21.1-nightly.26

--- src/main/java/io/github/vrchatapi/model/CalendarEventDiscovery.java
+++ src/main/java/io/github/vrchatapi/model/CalendarEventDiscovery.java
@@ -71,7 +71,7 @@ public class CalendarEventDiscovery {
   }
 
   /**
-   * Base64-encoded JSON:   type: object   properties:     dataSource:       type: string       enum:         - featured         - personalized     dataIndex:       type: integer       format: int32     phase:       type: string       enum:         - all         - live         - upcoming       description: see CalendarEventDiscoveryScope     asOf:       type: integer       format: int64       description: milliseconds since Unix epoch     paramHash:       type: string       format: string       description: Base64-encoded 256-bit hash of the original query parameters
+   * Base64-encoded JSON:   type: object   properties:     dataSource:       type: string       enum:         - featured         - personalized     dataIndex:       type: integer       format: int32     phase:       type: string       enum:         - all         - live         - upcoming       description: see CalendarEventDiscoveryScope     asOf:       type: integer       format: int64       description: milliseconds since Unix epoch     paramHash:       type: string       description: Base64-encoded 256-bit hash of the original query parameters
    * @return nextCursor
    */
   @javax.annotation.Nullable
